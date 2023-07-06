fb415b425d5825e0199bb7dd2c81d12d624fad78
ItsBash2021 <shahroozmehri17@gmail.com>
2023-07-06 12:35:58 -0700

    Added Data Structs for Items

---
+++ Source/VitalVale/Struct/ItemDataStructs.h
@@ -0,0 +1,100 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataTable.h"
+#include "ItemDataStructs.generated.h"
+
+UENUM()
+enum EItemQuality : uint8
+{
+	Common UMETA(DisplayName = "Common"),
+	Rare UMETA(DisplayName = "Rare"),
+	Epic UMETA(DisplayName = "Epic"),
+	Legendary UMETA(DisplayName = "Legendary"),
+	Special UMETA(DisplayName = "Special")
+};
+
+UENUM()
+enum EItemType : uint8
+{
+	Weapon UMETA(DisplayName = "Weapon"),
+	Armor UMETA(DisplayName = "Armor"),
+	Food UMETA(DisplayName = "Food"),
+	Miscellaneous UMETA(DisplayName = "Miscellaneous")
+};
+
+USTRUCT()
+struct FItemStatistics
+{
+	GENERATED_USTRUCT_BODY()
+
+	UPROPERTY(EditAnywhere)
+	float ArmorRating;
+	UPROPERTY(EditAnywhere)
+	float DamageValue;
+	UPROPERTY(EditAnywhere)
+	float RestoreAmount;
+};
+
+USTRUCT()
+struct FItemTextData
+{
+	GENERATED_USTRUCT_BODY()
+
+	UPROPERTY(EditAnywhere)
+	FText Name;
+	UPROPERTY(EditAnywhere)
+	FText Description;
+	UPROPERTY(EditAnywhere)
+	FText InteractionText;
+	UPROPERTY(EditAnywhere)
+	FText UsageText;
+};
+
+USTRUCT()
+struct FItemNumericData
+{
+	GENERATED_USTRUCT_BODY()
+
+	UPROPERTY(EditAnywhere)
+	int32 MaxStackSize;
+
+	UPROPERTY(EditAnywhere)
+	bool bIsStackable;
+};
+
+USTRUCT()
+struct FItemAssetData
+{
+	GENERATED_USTRUCT_BODY()
+
+	UPROPERTY(EditAnywhere)
+	UTexture2D* Icon;
+
+	UPROPERTY(EditAnywhere)
+	UStaticMesh* Mesh;
+};
+
+USTRUCT()
+struct FItemData : public FTableRowBase
+{
+	GENERATED_USTRUCT_BODY()
+
+	UPROPERTY(EditAnywhere, Category = "Item Data")
+	EItemType ItemType;
+
+	UPROPERTY(EditAnywhere, Category = "Item Data")
+	EItemQuality ItemQuality;
+
+	UPROPERTY(EditAnywhere, Category = "Item Data")
+	FItemStatistics ItemStatistics;
+
+	UPROPERTY(EditAnywhere, Category = "Item Data")
+	FItemTextData TextData;
+
+	UPROPERTY(EditAnywhere, Category = "Item Data")
+	FItemNumericData NumericData;
+
+	UPROPERTY(EditAnywhere, Category = "Item Data")
+	FItemAssetData AssetData;
+};
